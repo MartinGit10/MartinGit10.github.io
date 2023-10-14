fb3f6f271c2777ca30425f2718083b07cf808aed
MartinGit10 <147890378+MartinGit10@users.noreply.github.com>
2023-10-14 17:35:28 -0300

    Create multipower.js

---
+++ multipower.js
@@ -0,0 +1,7 @@
+// Archivo index.js
+
+// Este código se ejecutará cuando la página se cargue completamente.
+window.onload = function () {
+    // Imprime un mensaje en la consola del navegador.
+    console.log('¡Hola desde el archivo multipower.js en GitHub Pages!');
+};
